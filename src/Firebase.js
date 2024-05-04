3bda73f3210568a7b261c6cdc545b05457b1cd4c
AchitzkiRodrigo <168635093+AchitzkiRodrigo@users.noreply.github.com>
2024-05-04 08:18:51 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_APP_ID}`,
+  apiKey: "AIzaSyBnEK0Z9I3wKgXDIrWt4NlPAu_51tD9qH8",
+  authDomain: "telalogin2-a1cc2.firebaseapp.com",
+  projectId: "telalogin2-a1cc2",
+  storageBucket: "telalogin2-a1cc2.appspot.com",
+  messagingSenderId: "217806998589",
+  appId: "1:217806998589:web:0687ebd04f381f3ce16e54"
 };
 
 if(!firebase.apps.length){
